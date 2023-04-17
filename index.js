79ba1010d1db2c5bc7f6792404aa08c8cb23aea1
ahamed lameen <ahamedlameen@gmail.com>
2023-04-17 10:09:31 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.get('/getList', (req, res) => {
-    res.send('Show the list of data');
-});
 
 app.post('/login', (req, res) => {
     const requestData = req.body;
